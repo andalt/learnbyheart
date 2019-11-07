4bca80363e61d54832500bc951ef8e9714ef9c40
Миронов Андрей <and@smarthead.ru>
2019-11-07 15:05:27 +0300

    useEffect dependency

--- src/js/app/App.tsx
+++ src/js/app/App.tsx
@@ -1,18 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import logo from '../../logo.svg';
 import styles from './App.module.css';
 import './App.module.css';
 import Papa from 'papaparse';
 
 function App() {
-    type Words = [{
+    type Word = {
         type: string;
         english:  string;
         russian: string;
-    }];
+    };
 
+    type Words = {
+        [key:string]: Word;
+    };
 
-    const [words, setWords] = useState<Words | null>(null);
+    const [words, setWords] = useState<Words[]>([]);
+
+    const getWordsPreposition = useCallback(async () => {
+        const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNHkNmfpLyS_5gnrhTo5qayBIDx0ZMnVD560tq0vYSJEG2blf8hvE7m100U4BOZr8ZFv8rRH0FoUPP/pub?gid=142624304&single=true&output=csv';
+        const response = await fetch(url);
+
+        return await response.text();
+    }, []);
 
     useEffect(() => {
         getWordsPreposition()
@@ -21,17 +31,17 @@ function App() {
                     header: true,
                 });
 
-                // setWords(object.data);
-                console.log(object);
+                console.log(123);
+
+                setWords(object.data);
             });
-    }, [words]);
 
-    const getWordsPreposition = async() => {
-        const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNHkNmfpLyS_5gnrhTo5qayBIDx0ZMnVD560tq0vYSJEG2blf8hvE7m100U4BOZr8ZFv8rRH0FoUPP/pub?gid=142624304&single=true&output=csv';
-        const response = await fetch(url);
+        return () => {
+            setWords([]);
+        };
+    }, [getWordsPreposition]);
 
-        return await response.text();
-    };
+    console.log(words);
 
     return (
         <div className={styles.app}>
